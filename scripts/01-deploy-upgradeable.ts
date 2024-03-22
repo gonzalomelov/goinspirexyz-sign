278c3a6a34c66cef00d1747321092d5e7f1a4dda
Jack Xu <jxu@ethsign.xyz>
2024-03-22 11:59:25 -0700

    Demo

--- scripts/01-deploy-upgradeable.ts
+++
@@ -1,9 +0,0 @@
-import {ethers, upgrades} from 'hardhat'
-
-async function main() {
-    const Factory = await ethers.getContractFactory('${ContractName}')
-    const instance = await upgrades.deployProxy(Factory, [])
-    await instance.waitForDeployment()
-}
-
-void main()
